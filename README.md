48452275e51208350e14a682b69380a4fc919bd1
z2z63 <vituralfuture@gmail.com>
2024-08-11 23:27:29 +0800

    feat: 适配AstrBot插件的更新

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
-# helloworld
+# VChat provides wechat platform support for AstrBot
+[VChat](https://github.com/z2z63/VChat) 为 [AstrBot](https://github.com/Soulter/AstrBot) 提供了微信平台支持
+# VChat
+本插件使用 [VChat](https://github.com/z2z63/VChat) 接入微信，使用 UOS 微信 API ，相比网页版微信解除了以下限制
+- 不需要在2017年之前登录过微信网页端
+- 不需要开通支付功能
+- 新注册的微信号也能使用
 
-AstrBot 插件模板
+# 如何使用
+1. 新注册一个微信号
+2. 安装VChat插件
+3. 在AstrBot仪表盘中启用VChat插件
+4. 完成扫码登录
+5. enjoy
 
-A template plugin for AstrBot plugin feature
+# 已知限制
+1. [VChat](https://github.com/z2z63/VChat) 目前只兼容python3.10及以上
+2. 管理员身份暂未实现，所有消息都被认为是普通用户
 
-# 支持
+# 反馈
+请使用github issue  
+欢迎PR
 
-[帮助文档](https://astrbot.soulter.top/center/docs/%E5%BC%80%E5%8F%91/%E6%8F%92%E4%BB%B6%E5%BC%80%E5%8F%91/
-)
+# 重要
+使用本项目默认用户已经阅读并知悉[微信个人账号使用规范](https://weixin.qq.com/cgi-bin/readtemplate?&t=page/agreement/personal_account&lang=zh_CN)
+
+推荐单独注册一个微信号，并严格限制其用途，严格遵守《计算机软件保护条例》（2013修订）第十七条规定，禁止商用
+> 为了学习和研究软件内含的设计思想和原理，通过安装、显示、传输或者存储软件等方式使用软件的，可以不经软件著作权人许可
+
+一切由于使用本项目造成的后果由使用者自行承担
+
+如有侵权，请联系作者删除
--- main.py
+++ main.py
@@ -1,32 +1,127 @@
+import base64
+import io
+from pathlib import Path
+from typing import Any, List
+
+from nakuru.entities.components import BaseMessageComponent, Plain, Image
+
+from type.astrbot_message import AstrBotMessage, MessageMember, MessageType
+
 flag_not_support = False
+
+from vchat import Core
+import vchat.model as model
+
 try:
     from util.plugin_dev.api.v1.bot import Context, AstrMessageEvent, CommandResult
-    from util.plugin_dev.api.v1.config import *
+    from util.plugin_dev.api.v1.config import load_config, put_config
+    from util.plugin_dev.api.v1.platform import Platform
+    from util.plugin_dev.api.v1.message import MessageHandler
+    from util.plugin_dev.api.v1.register import register_platform
 except ImportError:
     flag_not_support = True
     print("导入接口失败。请升级到 AstrBot 最新版本。")
 
-'''
-注意以格式 XXXPlugin 或 Main 来修改插件名。
-提示：把此模板仓库 fork 之后 clone 到机器人文件夹下的 addons/plugins/ 目录下，然后用 Pycharm/VSC 等工具打开可获更棒的编程体验（自动补全等）
-'''
+
+class WechatPlatform(Platform):
+    def __init__(self, context: Context, config: dict[str, bool]):
+        super().__init__()
+        self.core = Core()
+        self.config = config
+        self.context = context
+        context.register_task(self.run(), "vchat-core-run")
+
+    async def run(self):
+        await self.core.init()
+        enable_cmd_qr = self.config.get("enable_cmd_qr", False)
+        hot_reload = self.config.get("hot_reload", True)
+        await self.core.auto_login(hot_reload=hot_reload, enable_cmd_qr=enable_cmd_qr)
+        await self.core.send_msg("Hello, filehelper", to_username="filehelper")
+
+        @self.core.msg_register(msg_types=model.ContentTypes.TEXT,
+                                contact_type=model.ContactTypes.USER | model.ContactTypes.CHATROOM)
+        async def _(msg: model.Message):
+            await self.handle_msg(self.convert_message(msg))
+
+        await self.core.run()
+
+    @staticmethod
+    def convert_message(msg: model.Message) -> AstrBotMessage:
+        assert isinstance(msg.content, model.TextContent)
+        amsg = AstrBotMessage()
+        amsg.message = [Plain(msg.content.content)]
+        sender = msg.chatroom_sender or msg.from_
+        amsg.sender = MessageMember(sender.username, sender.nickname)
+        amsg.message_str = msg.content.content
+        amsg.message_id = msg.message_id
+        if isinstance(msg.from_, model.User):
+            amsg.type = MessageType.FRIEND_MESSAGE
+        elif isinstance(msg.from_, model.Chatroom):
+            amsg.type = MessageType.GROUP_MESSAGE
+        else:
+            assert False, '不处理公众号'
+        amsg.raw_message = msg
+        session_id = msg.from_.username + "$$" + msg.to.username
+        if msg.chatroom_sender is not None:
+            session_id += '$$' + msg.chatroom_sender.username
+        amsg.session_id = session_id
+        return amsg
+
+    async def reply_msg(self, message: AstrBotMessage, result_message: List[BaseMessageComponent]):
+        assert isinstance(message.raw_message, model.Message)
+        username = message.raw_message.from_.username
+        # username = message.sender.user_id
+        if isinstance(result_message, str):
+            result_message = [Plain(result_message)]
+        await self._send_message(username, result_message)
+
+    async def send_msg(self, target: Any, result_message: CommandResult):
+        raise NotImplementedError
+
+    async def _send_message(self, username: str, result_message: List[BaseMessageComponent]):
+        plain_text = ""
+        for i in result_message:
+            if isinstance(i, Plain):
+                plain_text += i.text
+            elif isinstance(i, Image):
+                if i.path is not None:
+                    image_path = i.path
+                    await self.core.send_image(username, Path(image_path))
+                else:
+                    image_bytes = base64.b64decode(i.file[9:])
+                    image = io.BytesIO(image_bytes)
+                    await self.core.send_image(username, fd=image)
+
+        if plain_text != "":
+            await self.core.send_msg(plain_text, username)
+
+    async def handle_msg(self, message: AstrBotMessage):
+        # role = "admin" if message.sender.user_id in self.admin else "member"
+        role = "member"
+        ame = AstrMessageEvent.from_astrbot_message(message, self.context, "wechat", message.session_id, role)
+        message_result = await self.context.message_handler.handle(ame)
+        if not message_result:
+            return
+        await self.reply_msg(message, message_result.result_message)
+
+
 class Main:
     """
     AstrBot 会传递 context 给插件。
-    
+
     - context.register_commands: 注册指令
     - context.register_task: 注册任务
     - context.message_handler: 消息处理器(平台类插件用)
     """
+
     def __init__(self, context: Context) -> None:
         self.context = context
-        self.context.register_commands("helloworld", "helloworld", "内置测试指令。", 1, self.helloworld)
+        put_config("VChat", "使用vchat插件", "enable", False, "打开此开关以启用vchat插件")
+        put_config("VChat", "在终端打印二维码", "enable_cmd_qr", False,
+                   "如果在docker容器或服务器等无桌面环境下运行，请打开此开关")
+        put_config("VChat", "启用热重载", "hot_reload", True, "热重载允许几小时内无需扫码直接登录")
 
-    """
-    指令处理函数。
-    
-    - 需要接收两个参数：message: AstrMessageEvent, context: Context
-    - 返回 CommandResult 对象
-    """
-    def helloworld(self, message: AstrMessageEvent, context: Context):
-        return CommandResult().message("Hello, World!")
+        config = load_config("VChat")
+        if config.get("enable", False):
+            self.platform = WechatPlatform(context, config)
+            register_platform('wechat', context, self.platform)
